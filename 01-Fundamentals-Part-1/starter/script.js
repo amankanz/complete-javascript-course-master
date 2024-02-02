e7a19924fe759537a97fc415339231196844c86b
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-02-02 13:15:40 +0300

    Data type in JS

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,12 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 1);
 
 console.log("John Smith"); // "John Smith is a Value
 console.log(23); // 23 is a Value
 
-let firstName = "John Smith"; // Declaring a variable firstName and assign to it a Value John Smith
-console.log(firstName); // "John Smith is a Value
+// let firstName = "John Smith"; // Declaring a variable firstName and assign to it a Value John Smith
+// console.log(firstName); // "John Smith is a Value
 
 // const PI = 3.1415;
 
@@ -13,3 +14,50 @@ console.log(firstName); // "John Smith is a Value
 let myFirstJob = "JS Coder";
 let myCurrentJob = "Teacher";
 console.log(myFirstJob); // JS Coder
+*/
+
+// DATA TYPE
+// Primitive Data Type
+// Number, String, Boolean, Undefined, Null, Symbol and BigInt
+
+// Number (always called floating point numbers)
+let age = 25;
+
+// String (used for text)
+let firstName = "Zak";
+
+// Boolean (used for taking decisions)
+let fullName = true;
+
+// Undefined (empty Value taken by a variable that is not yet defined)
+let children; // variable declared without a Value assigned to it.
+
+// Null (Also means empty Value)
+
+// Symbol (ES2015)
+// Value that is unique and cannot be changed
+
+// BigInt (ES2020)
+// Another Type for Numbers
+// Larger Integers that cannot be represented by the Number Type
+
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// // console.log(typeof "Zak");
+
+// javaScriptIsFun = "YES!";
+// console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1997;
+console.log(typeof year);
+
+// A BUG in JS
+// Never correct due to legacy reasons
+console.log(typeof null); // Null is same as undefined (empty Value) but the output is object
