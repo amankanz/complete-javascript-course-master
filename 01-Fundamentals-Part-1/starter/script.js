9db9347598f0da3e87cf3b7ed6496e45cbf42560
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-02-04 15:25:35 +0300

    operator precedence and coding challenge #1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -15,7 +15,8 @@ let myFirstJob = "JS Coder";
 let myCurrentJob = "Teacher";
 console.log(myFirstJob); // JS Coder
 */
-
+//////////////////////////////////////////////////
+/*
 // DATA TYPE
 // Primitive Data Type
 // Number, String, Boolean, Undefined, Null, Symbol and BigInt
@@ -61,3 +62,75 @@ console.log(typeof year);
 // A BUG in JS
 // Never correct due to legacy reasons
 console.log(typeof null); // Null is same as undefined (empty Value) but the output is object
+*/
+////////////////////////////////////////////
+/*
+// Lecture 10
+// Let, Const and Var
+
+// Using the let keyword
+let age = 30;
+age = 31; // the variable got reassigned/muted  during execution.
+
+// Using the const keyword
+const BIRTHYEAR = 1997; // the value of BIRTHYEAR can not be changed
+// BIRTHYEAR = 1999;
+
+// Can not declare empty variable with const
+// const JOB;
+
+// Using Var keyword
+var job = "Coder";
+job = "Student";
+
+// Create the variable to the global scope
+// lastName = "Zihalirwa"; // Bad Practice
+// console.log(lastName);
+*/
+
+// Lecture 11
+
+/////////////////////////////////////////
+/*
+// Lecture 12
+// Operator Precedence
+const NOW = 2037;
+const AGE_ZAK = NOW - 1997;
+const AGE_SARAH = NOW - 2018;
+
+console.log(NOW - 1997 > NOW - 2018);
+
+// Mathematical operators
+// console.log(25 - 10 - 5); // executed left-to-right
+
+// Assignment
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+// Calculate Average age
+const AVERAGE_AGE = (AGE_ZAK - AGE_SARAH) / 2;
+console.log(AGE_ZAK, AGE_SARAH, AVERAGE_AGE);
+*/
+////////////////////////////////////////////////
+
+// Lecture 13
+// CODING CHALLENGE #1
+// TEST DATA 1:
+const MARK_WEIGHT = 78;
+const MARK_HEIGHT = 1.69;
+const JOHN_WEIGHT = 92;
+const JOHN_HEIGHT = 1.95;
+
+// TEST DATA 2:
+// const MARK_WEIGHT = 95;
+// const MARK_HEIGHT = 1.88;
+// const JOHN_WEIGHT = 85;
+// const JOHN_HEIGHT = 1.76;
+
+// Calculate both their BMIs
+const MARK_BMI = MARK_WEIGHT / MARK_HEIGHT ** 2;
+const JOHN_BMI = JOHN_WEIGHT / JOHN_HEIGHT ** 2;
+const MARK_HIGHER_BMI = MARK_BMI > JOHN_BMI;
+
+console.log(MARK_BMI, JOHN_BMI, MARK_HIGHER_BMI);
