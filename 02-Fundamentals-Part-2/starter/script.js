da10529b8316c745088015d698b24b2bd133a508
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-02-16 01:29:21 +0300

    Intro to arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -108,5 +108,134 @@ const years_until_retirement = (birthYear, firstName) => {
 console.log(years_until_retirement(1996, "Ziha"));
 console.log(years_until_retirement(1999, "Ingrid"));
 */
+///////////////////////////////////////////////
 
+/*
 // Lecture 33
+// Function calling other functions
+function cut_fruit_pieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessors(apples, oranges) {
+  // Calling cut_fruit_pieces() then capturing
+  // its return value into apple_pieces
+  // and orange_pieces
+  const apple_pieces = cut_fruit_pieces(apples);
+  const orange_pieces = cut_fruit_pieces(oranges);
+
+  const juice = `Juice of ${apple_pieces} pieces of 🍎 and ${orange_pieces} pieces of 🍊`;
+  return juice;
+}
+console.log(fruitProcessors(2, 3));
+*/
+////////////////////////////////////////
+
+/*
+// Lecture 34
+// Reviewing functions
+const cal_age = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years_until_retirement = function (birthYear, firstName) {
+  const age = cal_age(birthYear);
+  const retirement = 45 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired!🎉`);
+    return -1;
+  }
+};
+
+console.log(years_until_retirement(1996, "Ziha"));
+console.log(years_until_retirement(1970, "Bob"));
+*/
+//////////////////////////////////////////
+
+/*
+// Lecture 35
+// CODING CHALLENGE #1
+// Arrow function to calculate the average of 3 scores
+const calc_average = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// check_winner function
+const check_winner = function (avgDolpgins, avgKoalas) {
+  if (avgDolpgins >= 2 * avgKoalas) {
+    return `Dolphins wins 🏆 (${avgDolpgins} vs ${avgKoalas})`;
+  } else if (avgKoalas >= 2 * avgDolpgins) {
+    return `Koalas wins 🏆 (${avgKoalas} vs ${avgDolpgins})`;
+  } else {
+    return `No team wins...`;
+  }
+};
+
+// TEST DATA 1: Dolphins 44, 23 and 71. Koalas score 65, 54 and 49
+
+// Calculate the average for both teams
+const avg_score_dolphins = calc_average(44, 23, 71);
+const avg_score_koalas = calc_average(65, 54, 49);
+console.log(check_winner(avg_score_dolphins, avg_score_koalas));
+
+// TEST DATA 2: Dolphins 85, 54 and 41. Koalas score 23, 34 and 27
+const avg_score_dolphins_2 = calc_average(85, 54, 41);
+const avg_score_koalas_2 = calc_average(23, 34, 27);
+console.log(check_winner(avg_score_dolphins_2, avg_score_koalas_2));
+*/
+////////////////////////////////////
+
+/*
+// Lecture 36
+// Intro to Arrays
+// A big container in which we can store values and then reference them
+const friends = ["Amani", "MonIngrid", "Meshack", "Salleh", "Philipp"];
+console.log(friends);
+
+// Another of creating an Array
+const years = new Array(1996, 1997, 1998, 1999);
+console.log(years);
+
+// Accessing values inside the array
+console.log(friends[0]);
+console.log(friends[2]);
+
+// Length of the array(exact amount of elements inside the array)
+console.log(friends.length);
+
+// Get the last element of an array
+console.log(friends[friends.length - 1]);
+
+// Mutate the array
+friends[friends.length - 1] = "Clovis";
+console.log(friends);
+
+// Store values with different type
+const first_name = "Amani";
+const amani = [first_name, "Ziha", 2037 - 1997, "Coder", friends];
+console.log(amani);
+
+// Exercise
+const cal_age = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years_2 = [1997, 1999, 2000, 2010];
+
+const age_1 = cal_age(years_2[0]);
+const age_2 = cal_age(years_2[1]);
+const age_4 = cal_age(years_2[years_2.length - 1]);
+console.log(age_1, age_2, age_4);
+
+const ages = [
+  cal_age(years_2[0]),
+  cal_age(years_2[1]),
+  cal_age(years_2[years_2.length - 1]),
+];
+console.log(ages);
+*/
+///////////////////////////////////////
+
+// Lecture 37
