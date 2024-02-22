426b4d18c8958bc3b7ed7689bbee3a3c93a4702b
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-02-22 14:32:27 +0300

    Developer Skills Solving problem using Google.Stackoverflow.MDN

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,9 +1,91 @@
 // Remember, we're gonna use strict mode in all scripts now!
-"use strict";
+'use strict';
 
 // Lecture 49
 // Section Intro
 ////////////////////////////////////////
 
+/*
 // Lecture 50
 // Setting up Prettier and VSCode
+const x = '23';
+if (x === 23) console.log(23);
+
+const age = birthYear => {
+  return 2036 - birthYear;
+};
+
+console.log(); // cl
+console.log();
+
+// TODO: install Extensions
+// Image Preview by Kiss Tamas
+// Settings Sync by Shan Khan
+// TODO Highlight by Wayou Liu
+*/
+///////////////////////////////////////
+
+/*
+// Lecture 51
+// Installing Node.js and Setting Up
+// A Dev Environment
+// const calcAge = birthYear => 2037 - birthYear;
+
+// console.log(calcAge(1997));
+
+// TODO: Install npm package
+// npm install live-server -g
+// live-server
+*/
+///////////////////////////////
+
+// Lecture 52
+// Learning how to code is Hard but you can do it!
+/**
+ * Everything is awesome
+ *   - Study courses: underhand code, take challenges and notes
+ *
+ * Cliff of confusion
+ *  - Stay motivated! Keep writing lots of code on your own, no matter how bad
+ *
+ * Pit of despair
+ *  - Learn with other people, devs and beginners, and fix them
+ *
+ * Back to awesome
+ *  - Round up your skillset with best practices and tools ( git, testing, ...)
+ *
+ * JOB READY
+ *  - (But the learning never stops 😅)
+ */
+
+//////////////////////////////////////////////
+
+// Lecture 53
+// How to think like a Developer:
+// Become a Problem Solver!
+// 4 step to solve any problem:
+/**
+ * Understand the problem 100% Ask the right questions to get clear picture of the problem
+ *
+ * Divide and Conquer: Break the problem into smaller sub-problem
+ *
+ * Don't be afraid to do research: Google, MDN, StackOverflow
+ *
+ * For bigger problem, write pseudocode before writing the actual code
+ */
+
+// Tip: Develop a genuine curiosity and passions of understanding how things actually work in the world around you!
+
+///////////////////////////////////////
+
+// Lecture 54
+// Using Google, MDN and StackOverflow
+// PROBLEM:
+// "We work for a company building a smart home thermometer. Our most recent task is this: Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+
+// 2) Breaking up into sub-problems
+// Time stamp: 12:07
